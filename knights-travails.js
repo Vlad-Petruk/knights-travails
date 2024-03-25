268ee4f97a0c118c627cf414d278ea113a60c426
Vlad-Petruk <vladpetruk00@gmail.com>
2024-03-25 16:00:57 +0200

    with ChessSquare class and chessBoard

---
+++ knights-travails.js
@@ -0,0 +1,17 @@
+class ChessSquare {
+    constructor (vertical, horizontal) {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+    }
+}
+
+const chessBoard = [ 
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+ ]
